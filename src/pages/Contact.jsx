62081213466de415d185a6a4e1172108807e63b1
Arnav Khadkatkar <arnavkhadkatkar@gmail.com>
2026-08-06 14:04:11 +0530

    Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,22 @@
 import Layout from "../components/Layout";
+import PageHero from "../components/PageHero";
+import ContactForm from "../components/ContactForm";
+import ContactMap from "../components/ContactMap";
 
 const Contact = () => {
     return (
         <Layout>
-            <section className="min-h-screen" />
+
+            <PageHero
+                title="Start the Conversation"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et."
+                image="https://images.unsplash.com/photo-1600607687644-c7171b42498f?w=1800"
+            />
+
+            <ContactForm />
+
+            <ContactMap />
+
         </Layout>
     );
 };
