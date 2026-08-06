f28f7bc2531a41c2ed0a821af7537728af23068a
Arnav Khadkatkar <arnavkhadkatkar@gmail.com>
2026-08-06 14:03:23 +0530

    Home projects logic updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
-import Projects from "../components/Projects";
+import ProjectSection from "../components/ProjectSection";
 import Stats from "../components/Stats";
 import Promise from "../components/Promise";
+import projects from "../data/projects";
+
+import { useState } from "react";
 
 const Home = () => {
+     const [selected, setSelected] = useState(null);
+
      return (
           <Layout>
                <Hero />
-               <Projects />
+               <ProjectSection
+                    projectsData={projects.slice(0, 3)}
+                    selected={selected}
+                    setSelected={setSelected}
+               />
                <Stats />
                <Promise />
           </Layout>
